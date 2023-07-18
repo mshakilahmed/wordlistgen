9731a042dbb6f96e01e0923869eaff7485d01d18
mshakilahmed <87431024+mshakilahmed@users.noreply.github.com>
2023-07-18 13:37:10 +0000

    get meaning of word not found dictionary using google translate

--- src/lib/Page.svelte
+++ src/lib/Page.svelte
@@ -12,6 +12,7 @@
     $: text = "";
     $: list = [];
     $: result = [];
+    $: notFound = [];
     async function convertTextToList() {
         const originalText = document.getElementById("original-id");
         if (originalText == null) {
@@ -35,37 +36,75 @@ seg_list = list(seg_list)
         list = chineseOnly;
         for (let i = 0; i < list.length; i++) {
             const word = list[i];
-            console.log(word);
+            // console.log(word);
             await pyodide.runPython(
 `word_entry = d.lookup('` + word.trim() + `')
-if word_entry is None:
-	word_entry['trad'] = '` + word.trim() + `'
-	word_entry['definition_entries'] = [{'pinyin': '', 'definitions': ['']}]
-
-ch_sim = '<span class="ch-sim">' + word_entry.simp + '</span>'
-ch_trad = ''
-if word_entry.simp != word_entry.trad:
-  ch_trad = '<span class="ch-trad">〔' + word_entry.trad  + '〕</span>'
+not_found = []
 data = []
-for entry in word_entry.definition_entries:
-  pinyin = entry.pinyin
-  tone = ptc().convert_text(pinyin)
-  colored_pinyin = colorize_pinyin.colorized_HTML_string_from_string(tone)
-  if not colored_pinyin:
-    colored_pinyin = '<span class="pinYinWrapper"><span class="t5">' + tone + '</span></span>'
-  data.append({
-    "simplified": word_entry.simp,
-    "traditional": word_entry.trad,
-    "pinyin": colored_pinyin,
-    "definitions": entry.definitions
-  })
-print(data)
+if word_entry is None:
+    pin = pinyin_jyutping_sentence.pinyin("` + word.trim() + `")
+    pin = colorize_pinyin.colorized_HTML_string_from_string(pin)
+    not_found.append({
+        "simplified": "` + word.trim() + `",
+        "traditional": "` + word.trim() + `",
+        "pinyin": pin,
+        "definitions": ""
+    })
+else:
+    ch_sim = '<span class="ch-sim">' + word_entry.simp + '</span>'
+    ch_trad = ''
+    if word_entry.simp != word_entry.trad:
+        ch_trad = '<span class="ch-trad">〔' + word_entry.trad  + '〕</span>'
+    for entry in word_entry.definition_entries:
+        pinyin = entry.pinyin
+        tone = ptc().convert_text(pinyin)
+        colored_pinyin = colorize_pinyin.colorized_HTML_string_from_string(tone)
+        if not colored_pinyin:
+            colored_pinyin = '<span class="pinYinWrapper"><span class="t5">' + tone + '</span></span>'
+        data.append({
+            "simplified": word_entry.simp,
+            "traditional": word_entry.trad,
+            "pinyin": colored_pinyin,
+            "definitions": entry.definitions
+        })
 `
             );
             const chData = pyodide.globals.get("data").toJs();
-            console.log(chData[0]);
-            result = result.concat(Object.fromEntries(chData[0]));
+            const chNotFound = pyodide.globals.get("not_found").toJs();
+            if (chData != null && chData.length > 0) {
+                result = result.concat(Object.fromEntries(chData[0]));
+            }
+
+            if (chNotFound != null && chNotFound.length > 0) {
+                notFound = notFound.concat(Object.fromEntries(chNotFound[0]));
+                const meaning = await getMeaning(notFound[0]);
+                result = result.concat(meaning);
+            }
         }
+    }
+
+    function getMeaning(word) {
+        return new Promise(function (resolve, reject) {
+            const translateUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=zh-CN&tl=en-US&dt=t&q=${word.simplified}`;
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', translateUrl, true);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            let responseText = xhr.responseText;
+                            let res = JSON.parse(responseText);
+                            const data = {
+                                traditional: word.simplified,
+                                simplified: word.traditional,
+                                pinyin: word.pinyin,
+                                definitions: [res[0][0][0]]
+                            }
+                            resolve([data]);
+                        }
+                    }
+                }
+                xhr.send();
+        });
     }
 </script>
 
